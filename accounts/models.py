f96f7ce6ab14b79800cb4cda7c188d3906b0df40
Ismail El-Toukhy <ismail325m@gmail.com>
2023-09-08 19:14:14 +0300

    create Account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
+from uuid import uuid4
 
-# Create your models here.
+
+class Account(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=255)
+    balance = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        default=Decimal(0),
+        validators=[MinValueValidator(0)],
+    )
