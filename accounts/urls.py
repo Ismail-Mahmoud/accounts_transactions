7c7c09d1d55ccc61f128492f31d8da6d81411de7
Ismail El-Toukhy <ismail325m@gmail.com>
2023-09-09 13:57:07 +0300

    add accounts endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path(route="welcome/", view=views.welcome),
+    path(route="", view=views.AccountsList.as_view()),
+    path(route="<uuid:id>/", view=views.AccountInfo.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpRequest
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from .models import Account
+from .serializers import AccountSerializer
 
 
 def welcome(reauest: HttpRequest):
     return HttpResponse("Hello World!")
+
+
+class AccountInfo(APIView):
+    def get(self, request: Request, id):
+        """Get account by ID"""
+        account = get_object_or_404(Account, pk=id)
+        serializer = AccountSerializer(instance=account)
+        return Response(serializer.data)
+    
+    def patch(self, request: Request, id):
+        """Update account by ID"""
+        account = get_object_or_404(Account, pk=id)
+        serializer = AccountSerializer(instance=account, data=request.data, partial=True) # type: ignore
+        serializer.is_valid(raise_exception=True)
+        serializer.update(instance=account, validated_data=request.data)
+        return Response(data=serializer.data)
+
+
+class AccountsList(APIView):
+    def get(self, request: Request):
+        """List all accounts"""
+        queryset = Account.objects.all()
+        serializer = AccountSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request: Request):
+        """Create new account"""
+        serializer = AccountSerializer(data=request.data) # type: ignore
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+    
