8937c7aa92ef1c7ae82c1fa4b292f4f94c816a49
Ismail El-Toukhy <ismail325m@gmail.com>
2023-09-09 15:00:20 +0300

    add Transaction model

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,31 @@ from decimal import Decimal
 from uuid import uuid4
 
 
+DECIMAL_MAX_DIGITS = 10
+DECIMAL_PLACES = 2
+
+
 class Account(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=255)
     balance = models.DecimalField(
-        max_digits=8,
-        decimal_places=2,
+        max_digits=DECIMAL_MAX_DIGITS,
+        decimal_places=DECIMAL_PLACES,
         default=Decimal(0),
         validators=[MinValueValidator(0)],
     )
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.balance})"
+
+
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    src_account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name="+")
+    dest_account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name="+")
+    amount = models.DecimalField(
+        max_digits=DECIMAL_MAX_DIGITS,
+        decimal_places=DECIMAL_PLACES,
+        validators=[MinValueValidator(0)],
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
