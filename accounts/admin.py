d6b41cb533afc707d2ee54d37f19caafc43c1de5
Ismail El-Toukhy <ismail325m@gmail.com>
2023-09-10 19:06:10 +0300

    register admin models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "balance"]
+    list_per_page = 10
+    search_fields = ["name", "id"]
+
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["id", "src_account", "dest_account", "amount", "created_at"]
+    list_per_page = 10
+    list_select_related = True
+    autocomplete_fields = ["src_account", "dest_account"]
+    ordering = ["-created_at"]
+
